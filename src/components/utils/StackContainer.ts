2b7009da9a77623b0aef3fbaee5f294289f9b785
Anssi Herranen <anssi.herranen@iki.fi>
2019-03-30 22:39:44 +0200

    temp

--- src/components/utils/StackContainer.ts
+++ src/components/utils/StackContainer.ts
@@ -1,8 +1,30 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation'
+import { NavigationScreenOptions, createAppContainer, createStackNavigator } from 'react-navigation'
 
 import HomePage from '../HomePage'
 import MainInfoPage from '../MainInfoPage'
 import ProfilePage from '../ProfilePage'
+
+interface NavigationOptions {
+  (data: {
+    navigation: {
+      state: {
+        params: {
+          title: string,
+          content: string,
+          headerImage: {
+            url: string,
+            width: number,
+            height: number,
+          }
+        }
+      }
+    }
+  }): NavigationScreenOptions
+}
+
+const navigationOptions: NavigationOptions = ({ navigation }) => ({
+  title: `${navigation.state.params.name}'s Profile'`,
+})
 
 export default createAppContainer(createStackNavigator(
   {
@@ -14,6 +36,7 @@ export default createAppContainer(createStackNavigator(
     },
     MainInfoPage: {
       screen: MainInfoPage,
+      navigationOptions,
     },
     ProfilePage: {
       screen: ProfilePage,

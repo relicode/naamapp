6bbf82a5573b763f8e56cc42baad6f8541282021
Anssi Herranen <anssi.herranen@vincit.fi>
2019-06-28 14:47:50 +0300

    Fixed HomeScreen

--- src/components/HomeScreen/index.tsx
+++ src/components/HomeScreen/index.tsx
@@ -1,26 +1,31 @@
 import React, { Component } from 'react'
-import { Alert, ImageBackground, Text, TouchableHighlight, View } from 'react-native'
+import { Alert, Image, Text, TouchableHighlight, View } from 'react-native'
 import { NavigationEvents, NavigationScreenProps } from 'react-navigation'
 
 import { action } from '../../store'
 import { SYNC } from '../../store/dynamic-content/types'
-import commonStyles from '../../styles/common'
+import commonStyles, { GREEN } from '../../styles/common'
 import { MainInfoScreenRecord } from '../../utils/types/dynamic-content'
 import ListImageBackground from '../utils/ListImageBackground'
 import { MAIN_SCREEN_NAMES, MainScreenNames, screenNameMap } from '../utils/StackContainer'
 import landingScreenStyles from './styles'
 
 const { alert } = Alert
-const { rowStyle, columnStyle, textHeader } = commonStyles
-const { landingScreenButtonStyle } = landingScreenStyles
+const { columnStyle, textHeader } = commonStyles
+const {
+  landingScreenButtonStyle,
+  landingScreenImageSectionStyle,
+  landingScreenButtonSectionStyle,
+} = landingScreenStyles
 
 type HomeScreenProps = NavigationScreenProps
+type AlternatePressFn = () => void
 
 const backgroundImageMap: { [key in MainScreenNames]: any } = {
   Naamat20Years: require(`./navigation-icons/icon-Naamat20Years.png`),
   MainInfoScreenList: require(`./navigation-icons/icon-MainInfoScreenList.png`),
   PerformanceScreenList: require(`./navigation-icons/icon-PerformanceScreenList.png`),
-  PerformerScreenList: require(`./navigation-icons/icon-PerformerScreenList.png`),
+  NaamatCam: require(`./navigation-icons/icon-NaamatCam.png`),
 }
 
 export default class HomeScreen extends Component<HomeScreenProps> {
@@ -28,49 +33,52 @@ export default class HomeScreen extends Component<HomeScreenProps> {
     alert(screen.title, screen.content)
   }
 
-  public renderLandingScreenButton(text: string, screen: MainScreenNames) {
+  public renderLandingScreenButton(screen: MainScreenNames, alternatePressFn?: AlternatePressFn) {
     return (
-      <ImageBackground
-        source={backgroundImageMap[screen]}
-        style={
-          landingScreenButtonStyle
-          // width: screenWidth,
-          // height: imageHeight * screenWidth / imageWidth,
-          // justifyContent: 'center',
-          // alignItems: 'center',
-        }
+      <TouchableHighlight
+        key={screen}
+        underlayColor={GREEN}
+        activeOpacity={1.00}
+        style={ landingScreenButtonStyle }
+        onPress={alternatePressFn ? alternatePressFn : () => this.props.navigation.navigate(screen)}
       >
-        <TouchableHighlight
-          key={text}
-          underlayColor="white"
-          activeOpacity={0.95}
-          style={{ ...landingScreenButtonStyle, backgroundColor: 'rgba(0, 0, 0, 0.0)' }}
-          onPress={() => this.props.navigation.navigate(screen)}
-        >
+        <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+          <Image
+            source={backgroundImageMap[screen]}
+            style={{
+              width: 75,
+              height: 75,
+            }}
+          />
           <Text style={textHeader}>
-            {screenNameMap[text as MainScreenNames] || text}
-            </Text>
-        </TouchableHighlight>
-      </ImageBackground>
-
+            {screenNameMap[screen as MainScreenNames] || screen}
+          </Text>
+        </View>
+      </TouchableHighlight>
     )
   }
 
   public render() {
     return (
       <View style={columnStyle}>
-        <View style={rowStyle}>
+        <View style={landingScreenImageSectionStyle}>
           <ListImageBackground />
         </View>
-        <View style={rowStyle}>
+        <View style={landingScreenButtonSectionStyle}>
           <NavigationEvents
             onWillFocus={() => action({ type: SYNC })}
           />
           <View style={columnStyle}>
-            {MAIN_SCREEN_NAMES.slice(0, 2).map((s) => this.renderLandingScreenButton(s, s))}
+            {MAIN_SCREEN_NAMES.slice(0, 2).map((s) => this.renderLandingScreenButton(s))}
           </View>
           <View style={columnStyle}>
-            {MAIN_SCREEN_NAMES.slice(2).map((s) => this.renderLandingScreenButton(s, s))}
+            {MAIN_SCREEN_NAMES.slice(2).map((s) => (
+              s !== 'NaamatCam'
+                ? this.renderLandingScreenButton(s)
+                : this.renderLandingScreenButton(s, () => (
+                  alert('Naamakamera', 'Ota kiva naamakuva kaverista tai lipusta tai jotain. Ei ehditty toteuttaa.')
+                ))
+            ))}
           </View>
         </View>
       </View>

4fa95456e3667975621afddddd7dc5233898d53d
Anssi Herranen <anssi.herranen@vincit.fi>
2019-06-28 15:39:39 +0300

    Restyled MainInfoScreenList

--- src/components/MainInfoScreenList/index.tsx
+++ src/components/MainInfoScreenList/index.tsx
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
-import { TouchableHighlight } from 'react-native'
+import React, { Component, Fragment } from 'react'
+import { Text, TouchableHighlight, View } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
 import { NavigationScreenProps } from 'react-navigation'
 import { connect, MapStateToProps } from 'react-redux'
 
 import { ReduxStoreState } from '../../store'
+import commonStyles from '../../styles/common'
 import { MainInfoScreenRecord } from '../../utils/types/dynamic-content'
 import ListImageBackground from '../utils/ListImageBackground'
+import styles from './styles'
+
+const {
+  mainInfoScreenRow,
+  mainInfoScreenRowStart,
+  mainInfoScreenRowEnd,
+} = styles
+
+const { naamatView, textHeader } = commonStyles
 
 interface StateProps {
   mainInfoScreens: MainInfoScreenRecord[],
@@ -15,19 +25,31 @@ interface StateProps {
 class MainInfoScreenList extends Component<StateProps & NavigationScreenProps> {
   public render() {
     return (
-      <FlatList
-        data={this.props.mainInfoScreens.map((p) => ({ ...p, key: p.title }))}
-        renderItem={({ item }: { item: MainInfoScreenRecord }) => (
-          <TouchableHighlight
-            onPress={() => this.props.navigation.navigate('DynamicContentScreen', { screen: item, title: item.title })}
-          >
-            <ListImageBackground
-              headerImage={item.headerImage}
-              title={item.title}
-            />
-          </TouchableHighlight>
-        )}
-      />
+      <View style={naamatView}>
+        <ListImageBackground
+          title={'Info ja palvelut'}
+        />
+        <FlatList
+          data={this.props.mainInfoScreens.map((p) => ({ ...p, key: p.title }))}
+          renderItem={({ item }: { item: MainInfoScreenRecord }) => (
+            <TouchableHighlight
+              key={item.title}
+              onPress={
+                () => this.props.navigation.navigate('DynamicContentScreen', { screen: item, title: item.title })
+              }
+            >
+              <View style={mainInfoScreenRow}>
+                <View style={mainInfoScreenRowStart}>
+                  <Text style={textHeader}>{item.title}</Text>
+                </View>
+                <View style={mainInfoScreenRowEnd}>
+                  <Text style={textHeader}>{'>'}</Text>
+                </View>
+              </View>
+            </TouchableHighlight>
+          )}
+        />
+      </View>
     )
   }
 }

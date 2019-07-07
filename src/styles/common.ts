37f3743d4ff23156e54f9c6c986105b18fce7da1
Anssi Herranen <anssi.herranen@vincit.fi>
2019-07-07 22:33:43 +0300

    Fixed font and list views for Markdown views

--- src/styles/common.ts
+++ src/styles/common.ts
@@ -8,6 +8,7 @@ export const PINK = '#feb5ff'
 export const MAGENTA = '#f36ebd'
 
 const DEFAULT_FONT_FAMILY = Platform.OS === 'ios' ? 'Times New Roman' : 'serif'
+const DEFAULT_FONT_SIZE = 14
 
 const naamatView: ViewStyle = {
   flex: 1,
@@ -25,6 +26,7 @@ const columnStyle: ViewStyle = {
 }
 
 const naamatText: TextStyle = {
+  fontSize: DEFAULT_FONT_SIZE,
   fontFamily: DEFAULT_FONT_FAMILY,
   color: BLACK,
 }
@@ -64,177 +66,6 @@ const magentaBackground: ViewStyle = {
 const headerTitle = {
   fontFamily: DEFAULT_FONT_FAMILY,
 }
-
-/*
-export const styles = StyleSheet.create({
-  root: {},
-  view: {},
-  codeBlock: {
-    borderWidth: 1,
-    borderColor: '#CCCCCC',
-    backgroundColor: '#f5f5f5',
-    padding: 10,
-    borderRadius: 4,
-  },
-  codeInline: {
-    borderWidth: 1,
-    borderColor: '#CCCCCC',
-    backgroundColor: '#f5f5f5',
-    padding: 10,
-    borderRadius: 4,
-  },
-  del: {
-    backgroundColor: '#000000',
-  },
-  em: {
-    fontStyle: 'italic',
-  },
-  headingContainer: {
-    flexDirection: 'row',
-  },
-  heading: {},
-  heading1: {
-    fontSize: 32,
-  },
-  heading2: {
-    fontSize: 24,
-  },
-  heading3: {
-    fontSize: 18,
-  },
-  heading4: {
-    fontSize: 16,
-  },
-  heading5: {
-    fontSize: 13,
-  },
-  heading6: {
-    fontSize: 11,
-  },
-  hr: {
-    backgroundColor: '#000000',
-    height: 1,
-  },
-  blockquote: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    margin: 20,
-    backgroundColor: '#CCCCCC',
-  },
-  inlineCode: {
-    borderRadius: 3,
-    borderWidth: 1,
-    fontFamily: 'Courier',
-    fontWeight: 'bold',
-  },
-  list: {},
-  listItem: {
-    flex: 1,
-    flexWrap: 'wrap',
-    // backgroundColor: 'green',
-  },
-  listUnordered: {},
-
-  listUnorderedItem: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-  },
-
-  listUnorderedItemIcon: {
-    marginLeft: 10,
-    marginRight: 10,
-    ...Platform.select({
-      [PlatformEnum.IOS]: {
-        lineHeight: 36,
-      },
-      [PlatformEnum.ANDROID]: {
-        lineHeight: 30,
-      },
-    }),
-  },
-  listUnorderedItemText: {
-    fontSize: 20,
-    lineHeight: 20,
-  },
-
-  listOrdered: {},
-  listOrderedItem: {
-    flexDirection: 'row',
-  },
-  listOrderedItemIcon: {
-    marginLeft: 10,
-    marginRight: 10,
-    ...Platform.select({
-      [PlatformEnum.IOS]: {
-        lineHeight: 36,
-      },
-      [PlatformEnum.ANDROID]: {
-        lineHeight: 30,
-      },
-    }),
-  },
-  listOrderedItemText: {
-    fontWeight: 'bold',
-    lineHeight: 20,
-  },
-  paragraph: {
-    marginTop: 10,
-    marginBottom: 10,
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-  },
-  hardbreak: {
-    width: '100%',
-    height: 1,
-  },
-  strong: {
-    fontWeight: 'bold',
-  },
-  table: {
-    borderWidth: 1,
-    borderColor: '#000000',
-    borderRadius: 3,
-  },
-  tableHeader: {},
-  tableHeaderCell: {
-    flex: 1,
-    // color: '#000000',
-    padding: 5,
-    // backgroundColor: 'green',
-  },
-  tableRow: {
-    borderBottomWidth: 1,
-    borderColor: '#000000',
-    flexDirection: 'row',
-  },
-  tableRowCell: {
-    flex: 1,
-    padding: 5,
-  },
-  text: {},
-  strikethrough: {
-    textDecorationLine: 'line-through',
-  },
-  link: {
-    textDecorationLine: 'underline',
-  },
-  blocklink: {
-    flex: 1,
-    borderColor: '#000000',
-    borderBottomWidth: 1,
-
-  },
-  u: {
-    borderColor: '#000000',
-    borderBottomWidth: 1,
-  },
-  image: {
-    flex: 1,
-  },
-})
-*/
 
 export default StyleSheet.create({
   naamatView,
